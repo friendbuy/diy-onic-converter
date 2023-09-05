e27bd5c18fc614cf67cda71f7b5349c4d22789de
Robert Albarran <tbolt2fire@gmail.com>
2023-09-05 12:19:45 -0700

    add diyomicelement generator

--- public/diy-onic-converter.js
+++ public/diy-onic-converter.js
@@ -8,5 +8,22 @@ const diyOnicConverter = (textContentContainerSelector) => {
   /* Etc. etc. etc. your code etc. */
 };
 
+const PREFIX_LENGTH = 3;
+
+const createDIYOnicElement = (text) => {
+  const normalText = text.substring(PREFIX_LENGTH);
+  const emphasisText = text.substring(0, PREFIX_LENGTH);
+
+  const emphasisElement = document.createElement('b');
+  emphasisElement.textContent = emphasisText;
+
+  const element = document.createElement('span');
+  element.className = 'diyonic';
+  element.appendChild(emphasisElement);
+  element.append(normalText);
+
+  return element;
+};
+
 // Allow global access so that this can be executed from the console.
 window.diyOnicConverter = diyOnicConverter;
